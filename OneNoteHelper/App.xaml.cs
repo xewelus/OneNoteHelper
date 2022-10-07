1e87176e98568d5303d2f39d996866d42ec10c90
dinar <dinar.mobile@gmail.com>
2022-10-07 18:57:08 +0300

    CSCommon

--- OneNoteHelper/App.xaml.cs
+++ OneNoteHelper/App.xaml.cs
@@ -1,5 +1,5 @@
 using System.Windows;
-using CommonWpf.Classes.UI;
+using CommonWpf.Classes;
 
 namespace OneNoteHelper
 {
@@ -10,7 +10,7 @@ namespace OneNoteHelper
 	{
 		public App()
 		{
-			ExceptionHandler.Init();
+			AppInitializer.Initialize();
 		}
 	}
 }
